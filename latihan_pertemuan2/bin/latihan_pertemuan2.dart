47669a1df4bc8eeafacd441a1676ec1ad1c55016
ovi <145730060+arbinii@users.noreply.github.com>
2023-09-26 14:29:14 +0700

    Add files via upload

---
+++ latihan_pertemuan2/bin/latihan_pertemuan2.dart
@@ -0,0 +1,6 @@
+import 'package:latihan_pertemuan2/latihan_pertemuan2.dart' as latihan_pertemuan2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${latihan_pertemuan2.calculate()}!');
+}
+
---
+++ latihan_pertemuan2/test/latihan_pertemuan2_test.dart
@@ -0,0 +1,8 @@
+import 'package:latihan_pertemuan2/latihan_pertemuan2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
