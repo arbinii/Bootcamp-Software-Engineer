0ad8450eb6b1a500ce5e357006cdc30abb6034fb
ovi <145730060+arbinii@users.noreply.github.com>
2023-09-26 14:43:21 +0700

    Add files via upload

---
+++ latihan_pertemuan6/bin/latihan_pertemuan6.dart
@@ -0,0 +1,16 @@
+void main(){
+  String namalengkap = 'Novi Arbini';
+  String hobbi = 'Nonton Drakor';
+  int jumlahsaudara = 4;
+  String citacita = 'Apayah, belum kepikiran hehe :)';
+  var harapan = '''Harapan saya setelah selesai pelatihan ini, saya mampu meningkatkan skill saya 
+                  dibidang software engineer ini, setelah itu saya harap skill yang saya dapatkan dari pelatihan ini
+                  dapat saya terapkan dan saya gunakan untuk mendapatkan pekerjaan yang bagus.''';
+
+  print('Nama Lengkap : $namalengkap');
+  print('Hobbi : $hobbi');
+  print('Jumlah Saudara : $jumlahsaudara');
+  print('Cita-Cita : $citacita');
+  print('Harapan Setelah Selesai Palatihan Ini : $harapan');
+
+}
---
+++ latihan_pertemuan6/test/latihan_pertemuan6_test.dart
@@ -0,0 +1,8 @@
+import 'package:latihan_pertemuan6/latihan_pertemuan6.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
