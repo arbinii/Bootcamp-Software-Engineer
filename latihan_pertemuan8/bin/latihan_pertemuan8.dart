782cc589a57e0815ef3f3d024cfc96b58a73c66e
ovi <145730060+arbinii@users.noreply.github.com>
2023-09-26 14:46:41 +0700

    Add files via upload

---
+++ latihan_pertemuan8/bin/latihan_pertemuan8.dart
@@ -0,0 +1,5 @@
+import 'package:latihan_pertemuan8/latihan_pertemuan8.dart' as latihan_pertemuan8;
+
+void main(List<String> arguments) {
+  print('Hello world: ${latihan_pertemuan8.calculate()}!');
+}
