3293d6973a12fcc4ca798acf72f445ab2b76cb3c
ovi <145730060+arbinii@users.noreply.github.com>
2023-09-26 14:27:56 +0700

    Delete latihan_pertemuan2 directory

--- latihan_pertemuan2/bin/latihan_pertemuan2.dart
+++
@@ -1,6 +0,0 @@
-import 'package:latihan_pertemuan2/latihan_pertemuan2.dart' as latihan_pertemuan2;
-
-void main(List<String> arguments) {
-  print('Hello world: ${latihan_pertemuan2.calculate()}!');
-}
-
--- latihan_pertemuan2/test/latihan_pertemuan2_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:latihan_pertemuan2/latihan_pertemuan2.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
